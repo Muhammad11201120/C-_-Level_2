1d05bdaf6a623640b59ab1940c7c94b4d32567e6
mo <muha.11201120@gmail.com>
2024-04-22 09:43:13 +0300

    new lessons

---
+++ Linq_Example_ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Example_ConsoleApp
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            Book[] books = new Book[]
+            {
+                new Book { Name = "C#", Price = 100 },
+                new Book { Name = "C++", Price = 200 },
+                new Book { Name = "JavaScript", Price = 300 },
+                new Book { Name = "python", Price = 400 },
+                new Book { Name = "html", Price = 500 }
+            };
+
+            var myBooks = books.Where( book => book.Price > 200 );
+
+            foreach ( var b in myBooks )
+            {
+                Console.WriteLine( b.Name );
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionWithCustomAttributes_ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionWithCustomAttributes_ConsoleApp
+{
+    internal class Program
+    {
+        [AttributeUsage( AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true )]
+        public class DescriptionAttribute : Attribute
+        {
+            public string Description { get; }
+            public DescriptionAttribute( string description )
+            {
+                Description = description;
+            }
+        }
+        [Description( "This is a class" )]
+        public class MyClass
+        {
+            [Description( "This is a method" )]
+            public void MyMethod()
+            {
+            }
+        }
+        static void Main( string[] args )
+        {
+            //Get Type Of MyClass
+            Type type = typeof( MyClass );
+            //Get  Class-level Description Attribute
+            object[] attributes = type.GetCustomAttributes( typeof( DescriptionAttribute ), false );
+            foreach ( DescriptionAttribute attribute in attributes )
+            {
+                Console.WriteLine( attribute.Description );
+            }
+            //Get Method MyMethod
+            System.Reflection.MethodInfo method = type.GetMethod( "MyMethod" );
+            // Get method-level Description attributes
+            object[] methodAttributes = method.GetCustomAttributes( typeof( DescriptionAttribute ), false );
+            foreach ( DescriptionAttribute attribute in methodAttributes )
+            {
+                Console.WriteLine( attribute.Description );
+            }
+            Console.ReadLine();
+        }
+    }
+}
